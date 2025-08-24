f4f580335d33bae6dcdb03ad14fd7ad787d39366
mateus camanini <camanini28@gmail.com>
2025-08-23 22:46:22 -0300

    finalizacao AG3

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,13 +1,35 @@
-﻿namespace MauiAppMinhasCompras
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+﻿using MauiAppMinhasCompras.Helpers;
 
-            //MainPage = new AppShell();
-            MainPage = new NavigationPage(new Views.ListaProduto());
+namespace MauiAppMinhasCompras 
+{ 
+
+public partial class App : Application
+{
+
+        static SQLiteDatabaseHelper? _db;
+
+    public static SQLiteDatabaseHelper Db
+    {
+        get
+        {
+            if (_db == null)
+            {
+                string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "banco_sqlite_compras.dt3");
+
+                _db = new SQLiteDatabaseHelper(path);
+            }
+
+            return _db;
         }
     }
+
+    public App()
+    {
+        InitializeComponent();
+
+        //MainPage = new AppShell();
+        MainPage = new NavigationPage(new Views.ListaProduto());
+    }
+} 
 }
+
